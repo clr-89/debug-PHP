1130ac69c63840d6df755f15fb07e23a9009315a
clr-89 <74655761+clr-89@users.noreply.github.com>
2021-09-26 18:58:18 +0200

    Create tableauDebug.php

---
+++ tableauDebug.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Claire" => [
+        "city" => "Lormont",
+        "weapon" => "whip"
+    ]
+]; 
+
+var_dump($characters);
+
+?>
